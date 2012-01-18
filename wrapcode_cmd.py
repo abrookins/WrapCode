839121c9a1cbd25c2aba424341ab6e63d701b293
Andrew Brookins <a.m.brookins@gmail.com>
2012-01-17 16:33:29 -0800

    Initial commit

---
+++ wrapcode_cmd.py
@@ -0,0 +1,14 @@
+import sublime_plugin
+import codewrap
+
+
+class WrapCodeCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		col = self.view.settings().get('wrapcode_column', 80)
+		sel = self.view.sel()
+		wrapper = codewrap.CodeWrapper(width=col)
+
+		for region in sel:
+			wrapped = wrapper.fillParagraphs(self.view.substr(region))
+			self.view.replace(edit, region, wrapped)
+			
