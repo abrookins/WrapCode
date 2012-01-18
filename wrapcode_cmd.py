d08bc1dc1debae31a1a2dddd986d03b1d4d75cb4
Andrew Brookins <a.m.brookins@gmail.com>
2012-01-17 16:50:46 -0800

    Use spaces

--- wrapcode_cmd.py
+++ wrapcode_cmd.py
@@ -3,12 +3,12 @@ import codewrap
 
 
 class WrapCodeCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		col = self.view.settings().get('wrapcode_column', 80)
-		sel = self.view.sel()
-		wrapper = codewrap.CodeWrapper(width=col)
+    def run(self, edit):
+        col = self.view.settings().get('wrapcode_column', 80)
+        sel = self.view.sel()
+        wrapper = codewrap.CodeWrapper(width=col)
 
-		for region in sel:
-			wrapped = wrapper.fillParagraphs(self.view.substr(region))
-			self.view.replace(edit, region, wrapped)
-			
+        for region in sel:
+            wrapped = wrapper.fillParagraphs(self.view.substr(region))
+            self.view.replace(edit, region, wrapped)
+            
